7e20ee555b193d0b74c3fd5457cd1bd8c37c96ee
adoniar <ad522255@ucf.edu>
2025-02-19 15:06:50 -0500

    commented out express install

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const express = require('express');
+// const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3000;
