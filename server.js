47370b7e1af34310833e9bc8f769dcd23dfc942b
adoniar <ad522255@ucf.edu>
2025-02-19 15:07:50 -0500

    adjusted code

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 // const express = require('express');
 const mongoose = require('mongoose');
-const app = express();
+// const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
